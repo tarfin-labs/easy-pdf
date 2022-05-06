f9afb94f1f6735fde598b542ea0550ead895a7a0
frkcn <faarukcan@gmail.com>
2022-05-06 17:08:07 +0300

    test added for watermark

--- tests/MergeTest.php
+++ tests/MergeTest.php
@@ -8,11 +8,14 @@ use TarfinLabs\EasyPdf\EasyPdf;
 
 class MergeTest extends TestCase
 {
+    protected $file;
     protected $files;
 
     protected function setUp(): void
     {
         parent::setUp();
+
+        $this->file = __DIR__ . '/files/file.pdf';
 
         $this->files = [
             __DIR__.'/files/file.pdf',
@@ -24,6 +27,21 @@ class MergeTest extends TestCase
     public function it_can_merge_multiple_files_into_one_file()
     {
         $pdf = EasyPdf::merge($this->files)
+            ->content();
+
+        $finfo = new finfo(1040);
+        $buffer = $finfo->buffer($pdf);
+
+        $this->assertSame('application/pdf; charset=binary', $buffer);
+    }
+
+    /** @test */
+    public function it_can_add_watermark_into_the_file()
+    {
+        $easyPdf = new EasyPdf();
+        $pdf = $easyPdf->reset()
+            ->merge([$this->file])
+            ->addWatermark(__DIR__.'/files/watermark.png')
             ->content();
 
         $finfo = new finfo(1040);
